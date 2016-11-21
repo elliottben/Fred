51a9c9fd7f522d28bd01d0ce915548bbd07ba7f2
Ben Elliott <Ben Elliott>
2016-11-20 20:15:38 -0800

    pattern for parsing my reply through phone carrier identified

---
+++ text_to_csv.py
@@ -0,0 +1,10 @@
+#python to parse text file to csv file
+#pattern: message followed with a 1 instead of a 0, you sent message
+#if above holds true, then go one message above and copy the message instigating your reply
+
+import csv
+
+if __name__ == "__main__":
+    #import text file
+    #regex to parse file
+    #output to csv file
